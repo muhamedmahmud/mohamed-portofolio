f924a9314a586fa92f4a1d73ece08dd7b1a4d1bb
mohamed abdelwahab <101339472+muhamedmahmud@users.noreply.github.com>
2025-07-02 21:05:36 +0300

    navbar-done

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
-import Image from "next/image";
+//import Image from "next/image";
 import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]`">
-      
-      <div className="container  mx-auto py-4 px-12">
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <Navbar/>
+      <div className="container mt-24 mx-auto py-4 px-12">
       <HeroSection/>
+
       </div>
      
     </main>
